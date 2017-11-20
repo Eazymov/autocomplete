c99b6133945d86f66cc1a28bd151c26de5f16ce1
Eduard <eazymov@devvela.com>
2017-11-20 10:59:34 +0500

    WIP

--- src/api/Api-Spec.ts
+++ src/api/Api-Spec.ts
@@ -2,7 +2,7 @@ import API from './index'
 
 describe('API', function () {
 
-  it('should get cities', function (done) {
+  it('should get array of cities', function (done) {
     API.getCities().then((cities: Array<City>) => {
       expect(Array.isArray(cities)).toBeTruthy();
       
